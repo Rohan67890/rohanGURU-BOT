7613486e5ae10eac11fe7adb14833f1e116de019
Rohan67890 <143304323+Rohan67890@users.noreply.github.com>
2023-08-27 09:03:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94710593118', 'me', true],
+  ['94760388517', 'Rohan', true], 
   [''] 
 ] //Number of owners
 
